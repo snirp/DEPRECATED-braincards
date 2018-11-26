4510514c9e5e59b67540fe4d0e92b4598adceb93
Roy Prins <prinsroy@gmail.com>
2018-11-26 01:16:20 +0100

    Add question app

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice, Answer
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'time', 'correct', 'question')
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from ordered_model.models import OrderedModel
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    question = models.TextField()
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(OrderedModel):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.TextField()
+    correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    correct = models.BooleanField(default=True)
+    time = models.DateTimeField(auto_now_add=True)
+
